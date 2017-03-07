431059bff7874550722ddfc035601b996ddeeca0
andreasonny83 <andreasonny83@gmail.com>
2017-03-07 15:52:45 +0000

    fix($compile): lib folder removed from Git

--- lib/expandable-list.module.d.ts
+++
@@ -1,4 +0,0 @@
-import { ExpandableListComponent, ExpandableListItemComponent, ExpandableListStyler, ExpandableListDividerStyler } from './expandable-list.component';
-export declare class ExpandableListModule {
-}
-export { ExpandableListComponent, ExpandableListItemComponent, ExpandableListStyler, ExpandableListDividerStyler };
